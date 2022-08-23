31de69358fb54467cc49f63ed29ce0fdfc515c0e
Miriam Anyango <minaoanya@gmail.com>
2022-08-24 00:05:14 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
+class Book
+    attr_accessor :author
+    attr_accessor :genre
+    attr_accessor :page_count
+    attr_reader :title
+    
+    def initialize(title)
+        @title = title
+    end
 
+    def author=(author)
+        @author = author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+my_book = Book.new("And Then There Were None")
+my_book.title
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :color
+    attr_accessor :material
+    attr_accessor :condition
+    attr_accessor :size
+    attr_reader :brand
+    
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+    end
+end
+
+my_shoe = Shoe.new("Nike")
+my_shoe.brand
